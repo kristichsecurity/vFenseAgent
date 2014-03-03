eb6dc185907bfaf3b7d701fda3b86d2db9665708
Humberto Chacon <humchacon@gmail.com>
2014-03-03 12:57:27 -0500

    Removing unnecessary files.

--- agent/devtools/RVAgent_2_2_8/agent/plugins/rv/distro/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Python package to organize platform dependent classes/methods/functions.
-Package name 'distro' was chosen because 'platform' and 'os' are part of the
-python standard libs. And 'operatingsystem' is to long...
-"""
